199dce3bf8a9e580b48cc95f9a54529df2206384
周勇 <zhou.yong@ucsmy.com>
2017-07-31 17:54:20 +0800

    AlertDialog添加动画，从底部弹出 从底部消失

--- app/src/main/java/com/begin/androidmutiplex/MainActivity.java
+++ app/src/main/java/com/begin/androidmutiplex/MainActivity.java
@@ -1,19 +1,76 @@
 package com.begin.androidmutiplex;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.TransitionDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    private static Context mContext = null;
+    private Context mContext = null;
+
+    private Button mShowDialog = null;
+
+    private View mAlertView = null;
+
+    private ListView mListView = null;
+
+    private AlertDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mContext = this;
+        mShowDialog = (Button)findViewById(R.id.show_dialog);
+        mAlertView = LayoutInflater.from(this).inflate(R.layout.alert_dialog, null);
+        mListView = (ListView)mAlertView.findViewById(R.id.alert_list_view);
+        ArrayAdapter adapter = new ArrayAdapter(this, R.layout.select_pic_item,
+                getResources().getStringArray(R.array.select_pic_list));
+        mListView.setAdapter(adapter);
+        mShowDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog();
+            }
+        });
+    }
+
+    private void showDialog(){
+        if(null == dialog){
+            dialog = new AlertDialog.Builder(this)
+                    .setView(mAlertView)
+                    .setCancelable(true)
+                    .create();
+        }
+        Window window = dialog.getWindow();
+        window.setGravity(Gravity.BOTTOM);
+        window.setWindowAnimations(R.style.show_dialog_style_animation);
+        window.setBackgroundDrawable(new ColorDrawable(Color.WHITE));
+        if(!dialog.isShowing()){
+            dialog.show();
+        }
+//        window.getDecorView().setPadding(0, 0, 0, 0);
+        WindowManager.LayoutParams lp = window.getAttributes();
+        lp.width = WindowManager.LayoutParams.MATCH_PARENT;
+        lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
+        lp.horizontalMargin = 0;
+        window.setAttributes(lp);
     }
 }
